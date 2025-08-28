de37807e378336c8d02f0cbf46d985c078439aca
Vinicius Teixeira <vinist.002@gmail.com>
2025-08-28 14:44:38 -0300

    Implementada máscara de moeda(real) na coluna 'Preço'

--- src/app/app-module.ts
+++ src/app/app-module.ts
@@ -1,6 +1,9 @@
 import { NgModule, provideBrowserGlobalErrorListeners, provideZonelessChangeDetection } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { LOCALE_ID } from '@angular/core';
+import localePT from '@angular/common/locales/pt'
+
 import { AppRoutingModule } from './app-routing-module';
 import { App } from './app';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
@@ -9,7 +12,9 @@ import { Footer } from './components/footer/footer';
 import { ProductComponent } from './components/product/product';
 import { ProductsComponent } from './components/products/products';
 import { FormsModule } from '@angular/forms';
+import { registerLocaleData } from '@angular/common';
 
+registerLocaleData(localePT, 'pt');
 
 @NgModule({
   declarations: [
@@ -27,7 +32,8 @@ import { FormsModule } from '@angular/forms';
   ],
   providers: [
     provideBrowserGlobalErrorListeners(),
-    provideZonelessChangeDetection()
+    provideZonelessChangeDetection(),
+    {provide: LOCALE_ID, useValue: 'pt'}
   ],
   bootstrap: [App]
 })
