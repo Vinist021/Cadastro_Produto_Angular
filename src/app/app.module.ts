bbb4c58ac865e398a26822576bed2945e9ad9f29
Vinicius Teixeira <vinist.002@gmail.com>
2025-09-05 15:28:41 -0300

    Adicionada navbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import { ProductComponent } from './components/product/product.component';
 import { ProductsComponent } from './components/products/products.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { registerLocaleData } from '@angular/common';
+import { NavbarComponent } from './navbar/navbar.component';
 
 registerLocaleData(localePt, 'pt');
 
@@ -23,7 +24,8 @@ registerLocaleData(localePt, 'pt');
     HeaderComponent,
     ProductComponent,
     ProductsComponent,
-    FooterComponent
+    FooterComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
